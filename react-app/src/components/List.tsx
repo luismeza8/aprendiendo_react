3f746efc715b197d8e91e8eb106820eda90f991c
luismeza8 <angel841bv@gmail.com>
2024-12-07 15:22:58 -0700

    hooks

--- react-app/src/components/List.tsx
+++ react-app/src/components/List.tsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
+
 type props = {
   data: string[];
 };
 
 function List({ data }: props) {
-  const handleClick = (e: string) => {
-    console.log(e);
+  const [index, setIndex] = useState(1)
+
+  const handleClick = (i: number) => {
+    setIndex(i);
   }
   return (
     <>
       <ul className="list-group">
-        { data.map((elemento) => (
-          <li onClick={() => {handleClick(elemento)}} key={elemento} className="list-group-item">
+        { data.map((elemento, i) => (
+          <li 
+            onClick={() => {handleClick(i)}} 
+            key={elemento} 
+            className={`list-group-item ${index === i ? "active" : ""}`}
+          >
             {elemento}
           </li>
         )) }
