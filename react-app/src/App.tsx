7f46a0e12328bed4f79e40fb44263e7ccef5683d
luismeza8 <angel841bv@gmail.com>
2024-12-07 01:08:32 -0700

    first commit

---
+++ react-app/src/App.tsx
@@ -0,0 +1,7 @@
+import Card from './components/Card';
+
+function App() {
+  return <Card body={"Hola props"} />
+}
+
+export default App;
---
+++ react-app/src/Titulo.tsx
@@ -0,0 +1,7 @@
+function Titulo() {
+  const nombre = 'chanchito feliz'
+
+  return <h1>Hola {nombre}!</h1>
+}
+
+export default Titulo;
